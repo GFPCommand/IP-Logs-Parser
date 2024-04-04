89add3b33f120fd2c6015a1e471dcab20a854f20
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2024-04-04 23:36:48 +0300

    Добавьте файлы проекта.

---
+++ IP Parser/Parameters.cs
@@ -0,0 +1,10 @@
+﻿namespace IP_Parser
+{
+	class Parameters
+	{
+		public string FileLog { get; set; }
+		public string FileOut { get; set; }
+		public short AddressStart { get; set; } = 0;
+		public short AddressMask { get; set; } = 255;
+    }
+}
