8b44f4c45705001793ad849b199652da42ab2cfe
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2024-04-05 22:26:50 +0300

    Added cmd options

---
+++ IP Parser/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace IP_Parser
+{
+	class Options
+	{
+		[Option(longName: "file-log", Required = true, HelpText = "File log path")]
+		public string FileLog { get; set; }
+
+		[Option(longName: "file-out", Required = true, HelpText = "File out path")]
+		public string FileOut { get; set; }
+
+		[Option(longName: "address-start", Required = false, HelpText = "IP address start value")]
+		public int AddressStart { get; set; } = 0;
+
+		[Option(longName: "address-mask", Required = false, HelpText = "IP address mask")]
+		public int AddressMask { get; set; } = 255;
+    }
+}
--- IP Parser/Program.cs
+++ IP Parser/Program.cs
@@ -1,1 +1,15 @@
-﻿string configFile = "";
+﻿using CommandLine;
+using IP_Parser;
+
+Options opts = new();
+
+Parser.Default.ParseArguments<Options>(args)
+	.WithParsed(o =>
+	{
+		opts = o;
+	})
+	.WithNotParsed(e =>
+	{
+		
+	});
+
